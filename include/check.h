dbdf710a7332587b7bdbd167f80c4a4202d7896e
TungAnhNguyen5 <qtn5@sfu.ca>
2025-03-02 19:35:37 -0800

    final push for this project

--- include/check.h
+++ include/check.h
@@ -5,11 +5,6 @@
 #include <openssl/sha.h>
 #include <stddef.h>
 
-/*
- * The test cases internally use the following functions to check the
- * correctness of the blockchain implementation.
- */
-
 int set_difficulty(size_t num_zeros,
                    unsigned char difficulty[SHA256_DIGEST_LENGTH]);
 int check_block(size_t index, struct block *b, unsigned char data[DATA_SIZE],
